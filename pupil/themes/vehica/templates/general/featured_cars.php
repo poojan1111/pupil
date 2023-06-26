aadf591b42a90c6f5d7208cb62d924453edb25c6
Khushi983 <khushalinarola@strats360.com>
2023-06-26 10:52:46 +0530

    Update featured_cars.php

--- pupil/themes/vehica/templates/general/featured_cars.php
+++ pupil/themes/vehica/templates/general/featured_cars.php
@@ -127,7 +127,6 @@ $vehicaFeaturedCars = $vehicaCurrentWidget->getFeaturedCars();
                             <?php echo esc_html(vehicaApp('view_string')); ?>
                             <!-- <template>{{ carTabs.viewAllCount }} {{ carTabs.viewAllTitle }}</template> -->
                             <template>All</template>
-
                         </a>
                     </div>
                 <?php endif; ?>
